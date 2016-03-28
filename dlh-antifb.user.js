bcb7499513d58f1052b6d88eb9b3bf1774e1998f
Eldar <the.rooney.10@gmail.com>
2016-03-29 04:47:41 +0600

    Create dlh-antifb.user.js

---
+++ dlh-antifb.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         DLH.net AntiFB
+// @namespace    AntiFB
+// @version      0.1
+// @description  no need to like anything anymore
+// @author       Pepperooney
+// @include        https://www.dlh.net/*
+// @supportURL   	https://github.com/Pepperooney/dlh.net-AntiFB/issues
+// @updateURL    	https://raw.githubusercontent.com/Pepperooney/dlh.net-AntiFB/master/dlh-antifb.user.js
+// @downloadURL  	https://raw.githubusercontent.com/Pepperooney/dlh.net-AntiFB/master/dlh-antifb.user.js
+// @grant        none
+// ==/UserScript==
+//credits to https://www.reddit.com/u/popiazaza
+$('.hide.btn').show();
