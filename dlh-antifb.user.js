b390a821cd2584ef76a10fdbe8f47506d9d4826b
Eldar <the.rooney.10@gmail.com>
2016-03-30 13:50:50 +0600

    Update dlh-antifb.user.js

--- dlh-antifb.user.js
+++ dlh-antifb.user.js
@@ -5,6 +5,7 @@
 // @description  no need to like anything anymore
 // @author       Pepperooney
 // @include        https://www.dlh.net/*
+// @include        http://www.dlh.net/*
 // @supportURL   	https://github.com/Pepperooney/dlh.net-AntiFB/issues
 // @updateURL    	https://raw.githubusercontent.com/Pepperooney/dlh.net-AntiFB/master/dlh-antifb.user.js
 // @downloadURL  	https://raw.githubusercontent.com/Pepperooney/dlh.net-AntiFB/master/dlh-antifb.user.js
